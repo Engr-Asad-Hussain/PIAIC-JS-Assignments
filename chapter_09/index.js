2926f560c6cc53a9e3dbc89a81640e82140d5680
Engr-Asad-Hussain <asad.h1998@yahoo.com>
2021-01-17 17:50:32 +0500

    added chapter 8 assignment

---
+++ chapter_09/index.js
@@ -0,0 +1,6 @@
+const firstName = prompt("Enter your first name: ", "Mr.");
+const lastName = prompt("Enter your last name: ");
+
+let age = parseInt(prompt("Enter your age: ", "18"));
+
+console.log("Hello!\n" + firstName + " " + lastName + ", You are " + age +" years old" );
