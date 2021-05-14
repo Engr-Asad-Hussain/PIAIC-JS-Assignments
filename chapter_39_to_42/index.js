c9304e0d9674164a27c1643b01187bea417f696f
Engr-Asad-Hussain <asad.h1998@yahoo.com>
2021-05-14 10:27:13 +0500

    modified chapter 39 to 42

--- chapter_39_to_42/index.js
+++ chapter_39_to_42/index.js
@@ -4,15 +4,13 @@ let subjectMarks = 0;
 
 for (i=0; i<courses.length; i++) {
     subjectMarks = Number(prompt("Enter " + courses[i] + " marks: ", 0));
-    while (subjectMarks > 100) {
+    while (subjectMarks > 100 || Number.isNaN(subjectMarks)) { 
         alert("Invalid Marks\nPlease enter marks less than or equals to 100");
         subjectMarks = Number(prompt("Enter " + courses[i] + " marks: ", subjectMarks));
     }
     marks.push(subjectMarks);
 }
 
-// console.log(courses);
-// console.log(marks);
 
 function per(marks) {
     const totalMarks = 500;
@@ -27,14 +25,31 @@ function per(marks) {
 const percentage = per(marks);
 console.log(percentage);
 
-switch (percentage) {
-    case percentage > 90:
+
+switch (true) {
+    case (percentage >= 90):
         console.log("Percentage: " + percentage);
         console.log("Grade A+");
         break;
-    case 40:
+    case (percentage >= 75 && percentage <= 89):
+        console.log("Percentage: " + percentage);
+        console.log("Grade A");
+        break;
+    case (percentage >= 60 && percentage <= 74):
         console.log("Percentage: " + percentage);
         console.log("Grade B");
+        break;
+    case (percentage >= 45 && percentage <= 59):
+        console.log("Percentage: " + percentage);
+        console.log("Grade C");
+        break;
+    case (percentage >= 30 && percentage <= 44):
+        console.log("Percentage: " + percentage);
+        console.log("Grade D");
+        break;
+    case (percentage >= 0 && percentage <= 29):
+        console.log("Percentage: " + percentage);
+        console.log("Grade F");
         break;
     default:
         console.log("Invalid Data");
